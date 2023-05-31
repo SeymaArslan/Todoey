4bd3b380bf27a90262e25c8acaa95b85d0954b91
SeymaArslan <seymarslannn@gmail.com>
2023-05-31 20:47:06 +0300

    TableView delegate methods and accessories.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -17,7 +17,6 @@ class TodoListViewController: UITableViewController {
     }
     
     //MARK: - TableView Datasource Methods
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -26,10 +25,20 @@ class TodoListViewController: UITableViewController {
         
         // burada 2 farklı tableView geliyor V sembolu olan global ve bu UITableViewController'ı alt sınıflandırarak elde ettiğimz şey, main üzerindeki tableView dan IBOutlet oluşturmamızla aynı şeydir. L sembolu olan lokal değişken buradaki metot parametrelerinden biri olarak gelir. Projemizde hangisini seçtiğimizin bir önemi yok çünkğü ikisi de aynı şeye atıfta bulunuyor
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
         cell.textLabel?.text = itemArray[indexPath.row]
-        
         return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }   else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark  // belirli bir dizin yolunda bulunan hücreye bir referans alır, belirlediğimiz dizin yolu ise bu metotta seçtiğimiz ve aksesuar kullanmamıza izin verecek.
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true) // ----> seçili satırın sürekli olarak renkli gösterilmesini engelliyor niiiiiice
         
     }
 
